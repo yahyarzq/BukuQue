e5dc4977b1e9612264acc992a76ebf74935a6e92
yahyarzq <79823596+yahyarzq@users.noreply.github.com>
2021-06-16 11:50:20 +0700

    Added List Murid For Guru

--- lib/screens/forguru/home/daftarhari.dart
+++ lib/screens/forguru/home/daftarhari.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'daftarmurid.dart';
 
 class DaftarHari extends StatelessWidget {
   const DaftarHari({
@@ -7,67 +10,100 @@ class DaftarHari extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          '''Kelas 1A''',
-          overflow: TextOverflow.visible,
-          textAlign: TextAlign.left,
-          style: TextStyle(
-            height: 1.171875,
-            fontSize: 18.0,
-            fontFamily: 'Open Sans',
-            fontWeight: FontWeight.w300,
-            color: Color.fromARGB(255, 0, 0, 0),
-
-            /* letterSpacing: 0.0, */
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 70,
+          centerTitle: false,
+          title: Text(
+            '''BukuQue''',
+            overflow: TextOverflow.visible,
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              height: 1.171875,
+              fontSize: 36.0,
+              fontFamily: 'Damion',
+              fontWeight: FontWeight.w400,
+              color: Color.fromARGB(255, 255, 255, 255),
+              /* letterSpacing: 0.0, */
+            ),
           ),
         ),
-        Container(
-            child: ListView.separated(
-                itemBuilder: (context ,index) => ListWidget(namahari: '21 Mei 2021',),
-                separatorBuilder: (context,index) => Divider(height: 35,),
-                itemCount: 10
-            )
-        ),
+        body: Column(
+          children: [
+            Container(
+              height: 70,
+              padding: EdgeInsets.only(left: 20),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  '''Kelas 1A''',
+                  overflow: TextOverflow.visible,
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    height: 1.171875,
+                    fontSize: 20.0,
+                    fontFamily: 'Open Sans',
+                    fontWeight: FontWeight.w400,
+                    color: Color.fromARGB(255, 0, 0, 0),
 
-      ],
+                    /* letterSpacing: 0.0, */
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+                child: ListView.separated(
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) => ListWidgetDate(
+                          namahari: '21 Mei 2021', onTap: (){
+                      Navigator.of(context).push(MaterialPageRoute(builder: (context) => DaftarMurid()));
+                    },
+                        ),
+                    separatorBuilder: (context, index) => Divider(
+                          height: 35,
+                        ),
+                    itemCount: 10)),
+          ],
+        ),
+      ),
     );
   }
 }
 
-class ListWidget extends StatelessWidget {
+class ListWidgetDate extends StatelessWidget {
   final String namahari;
-  ListWidget({
-    Key key,
-    @required this.namahari
-  }) : super(key: key);
+  final Function onTap;
+
+  ListWidgetDate({Key key, @required this.namahari, @required this.onTap}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
+      onTap: onTap,
       child: Center(
         child: Container(
           height: 60,
           width: 350,
           decoration: BoxDecoration(
-              color: Colors.grey,
-              borderRadius: BorderRadius.circular(5.0)
-          ),
+              color: Colors.grey, borderRadius: BorderRadius.circular(5.0)),
           child: Align(
             alignment: Alignment.centerLeft,
-            child: Text(
-              '''$namahari''',
-              overflow: TextOverflow.visible,
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                height: 1.171875,
-                fontSize: 20.0,
-                fontFamily: 'Open Sans',
-                fontWeight: FontWeight.w400,
-                color: Color.fromARGB(255, 0, 0, 0),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text(
+                '''$namahari''',
+                overflow: TextOverflow.visible,
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  height: 1.171875,
+                  fontSize: 20.0,
+                  fontFamily: 'Open Sans',
+                  fontWeight: FontWeight.w400,
+                  color: Color.fromARGB(255, 0, 0, 0),
 
-                /* letterSpacing: 0.0, */
+                  /* letterSpacing: 0.0, */
+                ),
               ),
             ),
           ),
@@ -76,4 +112,3 @@ class ListWidget extends StatelessWidget {
     );
   }
 }
-
